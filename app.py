b5d182451aaf019aac261a1eb857d8147508f665
daviddruart <159968250+daviddruart@users.noreply.github.com>
2025-09-12 19:11:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def delete_book(book_id):
     return jsonify({"message": "Book deleted"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5454, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
